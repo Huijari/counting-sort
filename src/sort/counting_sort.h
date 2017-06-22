e3f448ba620702f3f45f07ae0d04d8a104cada7d
Alexandre Cesar <acmorcego@gmail.com>
2017-06-21 22:36:16 -0300

    Update counting_sort interface to use input_h

--- src/sort/counting_sort.h
+++ src/sort/counting_sort.h
@@ -5,15 +5,15 @@
 
 #include <stdint.h>
 
+#include "input.h"
+
 /**
  * Sort an array using the Counting Sort algorithm
  *
- * @param input Array of numbers to be sorted
- * @param legth Length of the input
- * @param range Range of the numbers of the input
+ * @param input Struct representing the data to be sorted
  *
  * @return New array with the items sorted
  */
-uint32_t* counting_sort(uint32_t* input, uint32_t length, uint32_t range);
+uint32_t* counting_sort(input_t input);
 
 #endif
