81e117e872e56e3b99e2604bc69ee0aad273dc31
Alexandre Cesar <acmorcego@gmail.com>
2017-06-21 23:00:07 -0300

    Update counting_sort implementation to use input_t

--- src/sort/counting_sort.c
+++ src/sort/counting_sort.c
@@ -6,25 +6,25 @@
 
 #include "counting_sort.h"
 
-uint32_t* counting_sort(uint32_t* input, uint32_t length, uint32_t range) {
-  uint32_t* count = calloc(sizeof(uint32_t), range);
+uint32_t* counting_sort(input_t* input) {
+  uint32_t* count = calloc(sizeof(uint32_t), input->range);
   assert(count != NULL);
-  memset(count, 0, sizeof(uint32_t) * range);
+  memset(count, 0, sizeof(uint32_t) * input->range);
 
-  for (int i = 0; i < length; i++)
-    count[input[i]]++;
+  for (int i = 0; i < input->length; i++)
+    count[input->values[i]]++;
 
   uint32_t total = 0;
-  for (int i = 0; i < range; i++) {
+  for (int i = 0; i < input->range; i++) {
     uint32_t oldCount = count[i];
     count[i] = total;
     total += oldCount;
   }
 
-  uint32_t* output = calloc(sizeof(uint32_t), length);
-  for (int i = 0; i < length; i++) {
-    output[count[input[i]]] = input[i];
-    count[input[i]]++;
+  uint32_t* output = calloc(sizeof(uint32_t), input->length);
+  for (int i = 0; i < input->length; i++) {
+    output[count[input->values[i]]] = input->values[i];
+    count[input->values[i]]++;
   }
 
   free(count);
